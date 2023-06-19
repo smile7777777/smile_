9ecafb39442735f7608e32bbf5454ea11d453190
smile7777777 <dylan.chen600@gmail.com>
2023-06-19 11:57:47 +0800

    alphabeta_advise

--- src/player/alphabeta.cpp
+++ src/player/alphabeta.cpp
@@ -3,7 +3,7 @@
 
 #include "../config.hpp"
 #include "../state/state.hpp"
-#include "../policy/random.hpp"
+#include "../policy/alphabeta.hpp"
 
 
 State* root;
@@ -42,7 +42,7 @@ void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
   while(true) {
     // Choose a random spot.
-    auto move = Random::get_move(root, 0);
+    auto move = AlphaBeta::get_move(root, 0);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
