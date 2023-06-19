3a8ff8c765cf7c134b93d72b4fd54074987967d1
smile7777777 <dylan.chen600@gmail.com>
2023-06-19 17:20:56 +0800

    finish_alphabeta

--- src/policy/alphabeta.cpp
+++ src/policy/alphabeta.cpp
@@ -11,10 +11,75 @@
  * @param depth You may need this for other policy
  * @return Move 
  */
-Move AlphaBeta::get_move(State *state, int depth){
+//int max(int a, int b) {return a > b ? a : b;}
+//int min(int a, int b) {return a < b ? a : b;}
+
+Move AlphaBeta::get_move(State *state, int depth, bool minimaxplayer){
   if(!state->legal_actions.size())
     state->get_legal_actions();
   
-  auto actions = state->legal_actions;
-  return actions[(rand()+depth)%actions.size()];
+  if(minimaxplayer){
+      int value = -1e9;
+      Move bestnext;
+      for(Move next : state->legal_actions){
+        State *child = state->next_state(next);
+        int child_value = alphabeta(child, depth-1 , false, -1e9, 1e9);
+
+        if(child_value > value) {
+            value = child_value;
+            bestnext = next;
+        }
+          delete child;
+      }
+        return bestnext;
+  } else {
+      int value = 1e9;
+      Move worstnext;
+
+      for(auto next : state->legal_actions){
+        State *child = state->next_state(next);
+        int child_value = alphabeta(child, depth-1 , true, -1e9, 1e9);
+
+        if(child_value < value){
+          value = child_value;
+          worstnext = next;
+        }
+          delete child;
+      }
+        return worstnext;
+  }
+}
+
+int AlphaBeta::alphabeta(State *state, int depth, bool minimaxplayer, int alpha, int beta){
+  //if((depth == 0 || !state->legal_actions.size()) && minimaxplayer)
+    //return state->evaluate1();
+  //if((depth == 0 || !state->legal_actions.size()) && !minimaxplayer)
+    //return state->evaluate2();
+    //role = Minimax::role;
+    if((depth == 0 || !state->legal_actions.size()))
+      return state->evaluate1();
+  
+  if(minimaxplayer){
+    int value = -1e9;
+    
+    for(Move next : state->legal_actions){
+      State *child = state->next_state(next);
+      value = std::max(value,alphabeta(child, depth - 1, false, alpha, beta));
+      alpha = std::max(alpha,value);
+      delete child;
+        if(alpha >= beta) break;
+    }
+      return value;
+  } else {
+    int value = 1e9;
+
+    for(Move next : state->legal_actions){
+      State* child = state->next_state(next);
+      value = std::min(value,alphabeta(child, depth - 1, true, alpha, beta));
+      beta = std::min(beta,value);
+      delete child;
+        if(beta <= alpha) break;
+    }
+      return value;
+  }
 }
--- src/policy/alphabeta.hpp
+++ src/policy/alphabeta.hpp
@@ -8,5 +8,6 @@
  */
 class AlphaBeta{
 public:
-  static Move get_move(State *state, int depth);
+  static Move get_move(State *state, int depth, bool player);
+  static int alphabeta(State *state, int depth, bool player, int alpha, int beta);
 };
